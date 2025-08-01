c531b9dd0e60fa7ccd6f6b190181ce24b2918cb0
João Felipe <joaofelipe343@hotmail.com>
2025-08-01 09:46:51 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+from mock_sheets import consulta_agencia
+
+app = Flask(__name__)
+
+@app.route('/consultar-dados', methods=['GET'])
+def get_dados_planilha():
+    # Pega os parâmetros da requisição HTTP (ex: /consultar-dados?planilha=MinhaPlanilha&aba=Sheet1&query=termo)
+    agency = request.args.get('agency')
+    bank = request.args.get('bank')
+    if not all([agency,bank]):
+        return jsonify({"error": "Parâmetros 'agency' e 'bank' são obrigatórios."}), 400
+
+    dados = consulta_agencia(agency,bank)
+
+    if dados is not None:
+        return jsonify(dados) # Retorna os dados como JSON
+    else:
+        return jsonify({"error": "Não foi possível consultar a planilha."}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True,port=8080)
---
+++ mock_sheets.py
@@ -0,0 +1,45 @@
+import gspread
+
+def consultar_planilha(nome_planilha, nome_aba,query):
+    try:
+        # Autentica usando as credenciais do arquivo JSON
+        gc = gspread.service_account(filename='key.json')
+
+        # Abre a planilha pelo nome
+        planilha = gc.open(nome_planilha)
+
+        # Seleciona a aba específica
+        aba = planilha.worksheet(nome_aba)
+
+        # Obtém todos os valores da aba
+        dados = aba.findall(query=query,case_sensitive=False,in_column=9,)
+
+        mock = []
+        if dados:
+            for value in dados:
+                mock.append(aba.row_values(value._row))    
+
+        return mock
+
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}")
+        return None
+
+def consulta_agencia(agency_number,bank_name:str):
+
+    my_dict = {}
+    i = 1
+
+    dados_da_minha_planilha = consultar_planilha("teste", "dados",agency_number)
+
+
+    if dados_da_minha_planilha:
+        for dados in dados_da_minha_planilha:
+            if(bank_name.upper() in dados[6]):
+                my_dict[str(i)] = {'municipio':dados[1],'uf':dados[3],'nome_agencia':dados[5],'banco':bank_name,'codigo':agency_number,'endereço':dados[9],'bairro':dados[10],'cep':dados[11]} 
+                i += 1      
+        return my_dict
+    
+    return None
+
+
