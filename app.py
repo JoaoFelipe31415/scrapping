6ec2fe756405068185e3105dbf8cbd9eacfbf14d
João Felipe <joaofelipe343@hotmail.com>
2025-08-09 13:11:26 -0300

    Bug fix

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from mock_sheets import consulta_agencia
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
+
 @app.route('/consultar-dados', methods=['GET'])
 def get_dados_planilha():
     # Pega os parâmetros da requisição HTTP (ex: /consultar-dados?planilha=MinhaPlanilha&aba=Sheet1&query=termo)
@@ -17,10 +18,3 @@ def get_dados_planilha():
         return jsonify(dados) # Retorna os dados como JSON
     else:
         return jsonify({"error": "Não foi possível consultar a planilha."}), 500
-
-if __name__ == '__main__':
-    # Em produção, o Render fornecerá a porta, então usamos gunicorn ou outro servidor WSGI
-    # Para testes locais, você pode usar a linha original ou Gunicorn
-    import os
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port, debug=True)
